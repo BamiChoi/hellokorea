0fac1be6160ca6a15df7376feb4ee50b8b8b26e6
beomseok.choi <cbs412@naver.com>
2022-03-10 20:58:48 +0900

    Private Route

--- client/src/Routes/LoggedInOnly.tsx
+++ client/src/Routes/LoggedInOnly.tsx
@@ -4,13 +4,13 @@ import { Navigate } from "react-router-dom";
 import { loggedInUser } from "reducers/auth";
 
 export interface IProtectedRoute {
-  children: JSX.Element;
+  children: React.ReactNode;
 }
 
 function LoggedInOnly({ children }: IProtectedRoute) {
   const user = useSelector(loggedInUser);
   console.log(user);
-  return user ? children : <Navigate to="/login" />;
+  return user ? <>{children}</> : <Navigate to="/login" />;
 }
 
 export default LoggedInOnly;
--- client/src/Routes/LogoutOnly.tsx
+++ client/src/Routes/LogoutOnly.tsx
@@ -1,15 +1,15 @@
+import React from "react";
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 import { loggedInUser } from "reducers/auth";
 
-export interface IProtectedRoute {
-  children: JSX.Element;
+export interface ProtectedRouteProps {
+  children: React.ReactNode;
 }
 
-function LogoutOnly({ children }: IProtectedRoute) {
+function LoggedInOnly({ children }: ProtectedRouteProps) {
   const user = useSelector(loggedInUser);
-  console.log(user);
-  return !user ? children : <Navigate to="/" />;
+  return !user ? <>{children}</> : <Navigate to="/login" />;
 }
 
-export default LogoutOnly;
+export default LoggedInOnly;
